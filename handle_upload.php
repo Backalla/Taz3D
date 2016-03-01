29455674da856597fac528388434916cffff287d
Tushar Pawar <tusharvijaypawar@gmail.com>
2016-03-01 19:14:06 +0530

    Parse manifest.xml and .slice file for print config and save in cws_file.xml

--- handle_upload.php
+++ handle_upload.php
@@ -26,15 +26,42 @@ if(isset($_FILES) && !empty($_FILES) && $_FILES['cws_file']['size']>0)
         // system("mv $file_upload_path cws/".$filename);
         exec("mv blank.png cws/".$filename);
         exec("sudo 7z e cws/".$filename.".cws -ocws/".$filename."/");
-        $fi = new FilesystemIterator("cws/".$filename."/", FilesystemIterator::SKIP_DOTS);
-        $slices = iterator_count($fi)-5;
+        $manifest_xml = simplexml_load_file("cws/".$filename."/manifest.xml");
+        $slices_xml_element = $manifest_xml->Slices->children();
+        $slices = count($slices_xml_element);
+
+        $slicing_config_xml = simplexml_load_file("cws/".$filename."/Voyager.slicing");
+        $blank_time = $slicing_config_xml->BlankTime;
+        $selected_resin = $slicing_config_xml->SelectedInk;
+        foreach ($slicing_config_xml->{'InkConfig'} as $resin)
+        {
+          if((string)$resin->Name == $selected_resin)
+          {
+            $slice_height = (int)((float)$resin->SliceHeight * 1000);
+            $layer_exposure = $resin->LayerTime;
+            $bottom_exposure = $resin->FirstLayerTime;
+            $number_bottom_layers = $resin->NumberofBottomLayers;
+          }
+        }
+        $print_time = 10+(($bottom_exposure/1000)*$number_bottom_layers)+(($blank_time/1000)*$slices)+(($layer_exposure/1000)*($slices-$number_bottom_layers));
         $xml = simplexml_load_file('cws_files.xml');
         $print = $xml->addChild('print');
         $print->addChild('filename', basename($_FILES['cws_file']['name'],".".$file_extension));
         $print->addChild('slices', $slices);
-        $print->addChild('print_time', $slices*4);
+        $print->addChild('print_time', gmdate("H:i:s",$print_time));
         $print->addChild('last_printed', time());
         $print->addChild('uploaded', time());
+        $print->addChild('blank_time',$blank_time);
+        $print->addChild('selected_resin',$selected_resin);
+        $print->addChild('slice_height',$slice_height);
+        $print->addChild('layer_exposure',$layer_exposure);
+        $print->addChild('bottom_exposure',$bottom_exposure);
+        $print->addChild('number_bottom_layers',$number_bottom_layers);
+
+
+
+
+
 
 
         file_put_contents('cws_files.xml', $xml->asXML());
--- test.php
+++ test.php
@@ -1,13 +1,21 @@
 <?php
 ini_set('display_errors', 1);
-$xml = simplexml_load_file("printer.xml");
-print_r($xml);
-$filename = $xml->filename;
-echo $filename;
-
-$xml->state="1";
-echo "<br>done<br>";
-
-echo $xml->asXML('printer.xml');
-
+$filename="3";
+$slices=321;
+$slicing_config_xml = simplexml_load_file("cws/".$filename."/Voyager.slicing");
+echo $blank_time = $slicing_config_xml->BlankTime;
+echo $selected_resin = $slicing_config_xml->SelectedInk;
+foreach ($slicing_config_xml->{'InkConfig'} as $resin)
+{
+	if((string)$resin->Name == $selected_resin)
+	{
+		echo $slice_height = (int)((float)$resin->SliceHeight * 1000);
+		echo $layer_exposure = $resin->LayerTime;
+		echo $bottom_exposure = $resin->FirstLayerTime;
+		echo $number_bottom_layers = $resin->NumberofBottomLayers;
+	}
+}
+echo "<br>Print time : ";
+$print_time = 10+(($bottom_exposure/1000)*$number_bottom_layers)+(($blank_time/1000)*$slices)+(($layer_exposure/1000)*($slices-$number_bottom_layers));
+echo gmdate("H:i:s",$print_time);
 ?>
