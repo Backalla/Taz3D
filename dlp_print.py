abae942d13cfbee620f00080ce6189bbfcf8b9f1
Tushar Pawar <tusharvijaypawar@gmail.com>
2016-03-30 20:44:43 +0530

    Adjusted for endstop and homing on pause, Changed port for Mega2560

--- dlp_print.py
+++ dlp_print.py
@@ -6,21 +6,11 @@ import Tkinter
 import Image, ImageTk
 import re
 from os import system
-Z_HEIGHT = -115
+Z_HEIGHT = -165
 
 def display_slice(image_path, layer_time):
   system("(sleep %d; kill $(pgrep fbi) ) &" %(layer_time/1000))
   system("fbi -noverbose '%s'" %(image_path))
-
-def set_printer_info(field,value):
-  printer_xml = ET.parse('printer.xml')
-  printer_root = printer_xml.getroot()
-  printer_field = printer_root.find(field)
-  printer_field.text=str(value)
-  printer_xml.write("printer.xml")
-
-
-
 
 def get_printer_info(field):
   printer_xml = ET.parse('printer.xml')
@@ -28,13 +18,12 @@ def get_printer_info(field):
   printer_field = printer_root.find(field).text
   return printer_field
 
-def send_gcode(gcode):
-  gcode = gcode.strip()
-  dlp_serial.write(gcode+'\n')
-  response=dlp_serial.readline()
-  dlp_serial.write("M400\n")
-  m400_response = dlp_serial.readline()
-  return response
+def increment_current_z(slice_height):  #increment current_z by slice height at every layer
+  set_printer_info('current_z',float(get_printer_info('current_z'))+(float(slice_height)/1000))
+
+
+def increment_elapsed_time(seconds):
+  set_printer_info('elapsed_time',float(get_printer_info('elapsed_time'))+float(seconds))
 
 
 def log(s):
@@ -43,13 +32,6 @@ def log(s):
   time_log = time_now+" : "+s+"\n"
   log_file.write(time_log)
   log_file.close()
-
-
-def increment_elapsed_time(seconds):
-  set_printer_info('elapsed_time',float(get_printer_info('elapsed_time'))+float(seconds))
-
-def increment_current_z(slice_height):  #increment current_z by slice height at every layer
-  set_printer_info('current_z',float(get_printer_info('current_z'))+(float(slice_height)/1000))
 
 def reset_printer():
   set_printer_info('state',1)
@@ -64,6 +46,32 @@ def reset_printer():
   set_printer_info('current_slice','')
   set_printer_info('filename','')
   set_printer_info('original_filename','')
+
+
+def send_gcode(gcode):
+  gcode = gcode.strip()
+  dlp_serial.write(gcode+'\n')
+  response=dlp_serial.readline()
+  dlp_serial.write("M400\n")
+  m400_response = dlp_serial.readline()
+  return response
+
+
+def set_printer_info(field,value):
+  printer_xml = ET.parse('printer.xml')
+  printer_root = printer_xml.getroot()
+  printer_field = printer_root.find(field)
+  printer_field.text=str(value)
+  printer_xml.write("printer.xml",xml_declaration=True,encoding='utf-8',method="xml")
+
+def shutter_close():
+  send_gcode("G1 X-10 F500")
+
+def shutter_open():
+  send_gcode("G1 X10 F500")
+
+
+
 # initialise all the files
 
 # Also set the state to 1 at startup to deal with accidental shutdowns
@@ -71,7 +79,7 @@ def reset_printer():
 
 # Open serial port to machine and initialise it
 try:
-  dlp_serial = serial.Serial('/dev/ttyUSB0',115200)
+  dlp_serial = serial.Serial('/dev/ttyACM0',115200)
   dlp_serial.write("\r\n\r\n")
   time.sleep(2)
   dlp_serial.flushInput()
@@ -123,7 +131,7 @@ def main():
         increment_elapsed_time(10)
         set_printer_info('current_z',Z_HEIGHT)
         # Header ends here
-
+        shutter_close()
         # Slices start
         all_slices = gcode_root.findall('slice')
         for cur_slice in all_slices:
@@ -140,16 +148,15 @@ def main():
           set_printer_info('current_slice',cur_slice_name)
           set_printer_info('completed_slices',cur_slice_no+1)
           # Display the image here
+          shutter_open()
           print time.time()
-
-
           display_slice('cws/'+cws_id+'/'+cur_slice_name,layer_time)
           print time.time()
-
+          shutter_close()
           # Check if paused
           if printer_state == "3":
             z=get_printer_info('current_z')
-            send_gcode("G1 Z"+str(abs(float(z)))+" F500")
+            send_gcode("G28 Z0")
             while get_printer_info('state') == "3":
               # Wait here until resumed
               time.sleep(0.1)
@@ -168,7 +175,19 @@ def main():
               print gcode
               send_gcode(gcode)
           increment_elapsed_time((float(layer_time)/1000)+(float(blanktime)/1000))
-          increment_current_z(get_printer_info('slice_height'))          
+          increment_current_z(get_printer_info('slice_height')) 
+
+
+        footer=gcode_root.find('footer').text
+        footer = footer.split('\n')
+        # send gcodes in Footer one by one
+        for gcode in footer:
+          if len(gcode) > 0:
+            print gcode
+            send_gcode(gcode)
+        increment_elapsed_time(10)
+        set_printer_info('current_z',0)
+        # Footer ends          
           # Slices end
   except KeyboardInterrupt:
     log("Printing stopped..")
--- test.py
+++ test.py
@@ -12,12 +12,12 @@ time.sleep(2)   # Wait for grbl to initialize
 s.flushInput()  # Flush startup text in serial input
  
 # Stream g-code to grbl
-
-l = raw_input().strip() # Strip all EOL characters for streaming
-print 'Sending: ' + l,
-s.write(l + '\n') # Send g-code block to grbl
-grbl_out = s.readline() # Wait for grbl response with carriage return
-print ' : ' + grbl_out.strip()
+while True:
+	l = raw_input().strip() # Strip all EOL characters for streaming
+	print 'Sending: ' + l,
+	s.write(l + '\n') # Send g-code block to grbl
+	grbl_out = s.readline() # Wait for grbl response with carriage return
+	print ' : ' + grbl_out.strip()
  
 # Wait here until grbl is finished to close serial port and file.
 raw_input("  Press <Enter> to exit and disable grbl.")
